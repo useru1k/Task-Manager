50d0c86429e685e440427b9a3f00a29e522d2f2b
Yuvan krishna P <143686903+useru1k@users.noreply.github.com>
2025-05-02 21:45:31 +0530

    Update Task Model Phase #1

--- src/model/Task.java
+++ src/model/Task.java
@@ -3,12 +3,69 @@ package model;
 import java.time.LocalDate;
 
 public class Task {
-
-	private int id;
+    private int id;
     private String title;
     private String description;
-    private String status;       
-    private LocalDate dueDate;  
+    private String status;
+    private LocalDate dueDate;
     private int userId;
-    
+
+    public Task() {}
+
+    public Task(int id, String title, String description, String status, LocalDate dueDate, int userId) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.status = status;
+        this.dueDate = dueDate;
+        this.userId = userId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
 }
